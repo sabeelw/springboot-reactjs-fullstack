f003e3ed99c2a1215499135d5036347266aee7f9
sabeelw <sabeelw.sk@gmail.com>
2023-11-25 13:33:58 +0100

    testing the github actions workflow

--- backend/src/main/java/com/koshur/springboot/Main.java
+++ backend/src/main/java/com/koshur/springboot/Main.java
@@ -18,11 +18,9 @@ import java.util.Objects;
 
 @SpringBootApplication
 public class Main {
-
     public static void main(String[] args){
 
        SpringApplication.run(Main.class, args);
-
     }
 
 }
